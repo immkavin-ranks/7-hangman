d48ac315feee47267a758bc6a12f3f534a459ca2
immkavin-ranks <immkavin.ofcl@gmail.com>
2023-09-20 23:54:43 +0530

    Keep track of the player's lives

--- main.py
+++ main.py
@@ -2,9 +2,78 @@
 
 import random
 
+stages = [
+    """
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+""",
+    """
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+""",
+    """
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+""",
+    """
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========""",
+    """
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+""",
+    """
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+""",
+    """
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+""",
+]
+
 word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 len_of_chosen_word = len(chosen_word)
+
+# TODO-1: - Create a variable called 'lives' to keep track of the number of lives left.
+# Set 'lives' to equal 6.
+lives = 6
 
 # Testing code
 print(f"Pssst, the solution is {chosen_word}.")
@@ -12,7 +81,6 @@ print(f"Pssst, the solution is {chosen_word}.")
 # Create blanks
 display = ["_"] * len_of_chosen_word
 
-# TODO-1: - Use a while loop to let the user guess again. The loop should only stop once the user has guessed all the letters in the chosen_word and 'display' has no more blanks ("_"). Then you can tell the user they've won.
 while "_" in display:
     guess = input("Guess a letter: ").casefold()
 
@@ -21,6 +89,20 @@ while "_" in display:
         if chosen_word[position] == guess:
             display[position] = guess
 
-    print(display)
+    # TODO-2: - If guess is not a letter in the chosen_word,
+    # Then reduce 'lives' by 1.
+    # If lives goes down to 0 then the game should stop and it should print "You lose."
+    if guess not in chosen_word:
+        lives -= 1
+
+    # Join all the elements in the list and turn it into a String.
+    print("".join(display))
+    print(stages[lives])
+
+    if lives == 0:
+        print("You lose.")
+        break
 else:
     print("You win!")
+
+    # TODO-3: - print the ASCII art from 'stages' that corresponds to the current number of 'lives' the user has remaining.
